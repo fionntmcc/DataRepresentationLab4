17c737cfd7c0ff40b9b6fd26f2d456dada93e617
Fionn McCarthy <g00414386@atu.ie>
2024-10-18 11:08:07 +0000

    form for creating movie

--- my-app/src/components/Create.js
+++ my-app/src/components/Create.js
@@ -1,6 +1,57 @@
+import { useState } from "react";
+
 // Reusable read component
 const Create = () => {
-    return <h1>Hello from the Create component</h1>;
+
+  const [title, setTitle, year, setYear, poster, setPosterURL] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title);
+    console.log(year);
+    console.log(poster);
+  }
+
+  return(
+    <div>
+      <h1>Hello from the Create component</h1>
+      <form onSubmit={handleSubmit}>
+        
+        <div>
+    	    <label>Add Movie Title: </label>
+          <input type="text"
+          className="form-control"
+          value={title}
+          onChange={(e) => {setTitle(e.target.value) }}
+          />
+        </div>
+
+        <div>
+    	    <label>Add Movie Year: </label>
+          <input type="text"
+          className="form-control"
+          value={year}
+          onChange={(e) => {setYear(e.target.value) }}
+          />
+        </div>
+
+        <div>
+    	    <label>Add Poster URL: </label>
+          <input type="text"
+          className="form-control"
+          value={poster}
+          onChange={(e) => {setPosterURL(e.target.value) }}
+          />
+        </div>
+
+        <div>
+          <input type="submit" value="Add Movie"/>
+        </div>
+        
+        
+      </form>
+    </div>
+  )
   };
   
   export default Create;
